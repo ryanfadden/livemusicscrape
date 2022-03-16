228c3b3e36005dd4709c69b483ececbd5f407b9a
ryanfadden <ryanfadden@gmail.com>
2022-03-15 20:45:31 -0600

    initial commit

---
+++ livemusicscrape.py
@@ -0,0 +1,20 @@
+# import sys
+# import requests
+# from bs4 import BeautifulSoup as Soup
+
+# webpage_response = requests.get('http://www.herbsbar.com/live-music-calendar-1')
+
+# webpage = (webpage_response.content)
+# parser = Soup(webpage, 'html.parser')
+
+
+#for herbs_music in parser:
+#     band_names = herbs_music.find_all('a', {'class':'eventlist-title-link'})
+#     dates = herbs_music.find_all('time', {'class':'event-date'})
+#     time = herbs_music.find_all('time', {'class':'event-time-12hr'})
+#     print (herbs_music)
+
+print ("hello")
+
+
+
