f34abe3152775c51d47b45d452e4f58e803c7225
ryanfadden <ryanfadden@gmail.com>
2022-03-16 14:55:30 -0600

    figured out script run issue

--- livemusicscrape.py
+++ livemusicscrape.py
@@ -1,20 +1,29 @@
-# import sys
-# import requests
-# from bs4 import BeautifulSoup as Soup
+import requests
+from bs4 import BeautifulSoup as Soup
 
-# webpage_response = requests.get('http://www.herbsbar.com/live-music-calendar-1')
+webpage_response = requests.get('http://www.herbsbar.com/live-music-calendar-1')
 
-# webpage = (webpage_response.content)
-# parser = Soup(webpage, 'html.parser')
+webpage = (webpage_response.content)
+parser = Soup(webpage, 'html.parser')
 
 
-#for herbs_music in parser:
+# for herbs_music in parser:
 #     band_names = herbs_music.find_all('a', {'class':'eventlist-title-link'})
 #     dates = herbs_music.find_all('time', {'class':'event-date'})
 #     time = herbs_music.find_all('time', {'class':'event-time-12hr'})
 #     print (herbs_music)
+#     print ("--------------------------")
 
-print ("hello")
+# showbandname = parser.find_all('a', {'class':'eventlist-title-link'})
+
+# for bandname in showbandname:
+#     print ()
+#     print (bandname)
 
 
+groupofwords = ["cat", "dog", "pig", "cow"]
+# print (groupofwords)
+# print (groupofwords[2])
 
+for word in groupofwords:
+    print (word)
