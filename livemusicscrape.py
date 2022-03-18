4981a76105181bc365130ef94ba1e0407ce05c54
ryanfadden <ryanfadden@gmail.com>
2022-03-17 21:01:05 -0600

    nothing much changed

--- livemusicscrape.py
+++ livemusicscrape.py
@@ -9,10 +9,12 @@ parser = Soup(webpage, 'html.parser')
 
 # for herbs_music in parser:
 #     band_names = herbs_music.find_all('a', {'class':'eventlist-title-link'})
-#     dates = herbs_music.find_all('time', {'class':'event-date'})
-#     time = herbs_music.find_all('time', {'class':'event-time-12hr'})
-#     print (herbs_music)
-#     print ("--------------------------")
+#     print (band_names)
+    # dates = herbs_music.find_all('time', {'class':'event-date'})
+    # time = herbs_music.find_all('time', {'class':'event-time-12hr'})
+
+band_names = parser.find_all('a', {'class':'eventlist-title-link'})
+print (band_names)
 
 # showbandname = parser.find_all('a', {'class':'eventlist-title-link'})
 
@@ -21,9 +23,11 @@ parser = Soup(webpage, 'html.parser')
 #     print (bandname)
 
 
-groupofwords = ["cat", "dog", "pig", "cow"]
-# print (groupofwords)
-# print (groupofwords[2])
+# groupofwords = ["cat", "dog", "pig", "cow"]
+# # print (groupofwords)
+# # print (groupofwords[2])
 
-for word in groupofwords:
-    print (word)
+# # for word in groupofwords:
+# #     print (word)
+
+# print (groupofwords)
